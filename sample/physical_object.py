37249ed29505c5b8ca491bcf584fcbd9236f2164
Francesco Maria Moneta <francesco.moneta@gmail.com>
2023-08-11 18:09:32 -0400

    Fixes to distance and math functions:

--- sample/physical_object.py
+++ sample/physical_object.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import random
+from math import sqrt
 
 from constants import Gconst
 
@@ -31,6 +32,10 @@ class Object_with_mass:
         if(len(self.trail_data) > self._max_trail_size ):
             np.delete(self.trail_data, 0)
 
+    def _calculate_distance(self, other: tuple_3):
+        d = pow(2, self.position.x * other.x) + pow(2, self.position.y * other.y) + pow(2, self.position.z * other.z)
+        return sqrt(d)
+
     def Display(self) -> None:
         print("Object: " + self.name + ", Mass: " + str(self.mass) + "KG")
         print("Position: ")
@@ -48,14 +53,16 @@ class Object_with_mass:
             else:
                 #F = G(m1 m2)/r^2
                 #A = F/m
-                quotient = Gconst * object.mass
-                ax = quotient / pow((self.position.x - object.position.x),2)
+                d = self._calculate_distance(object.position)
+                a = abs(Gconst * object.mass / pow(2.0, d))
+                # a^2 = pow(x2 - x1) + pow(y2 - y1) + pow(z2 - z1)
+                ax = a * abs((self.position.x - object.position.x)) / d
                 if self.position.x > object.position.x:
                     ax = ax * -1
-                ay = quotient / pow((self.position.y - object.position.y),2)
+                ay = a * abs((self.position.y - object.position.y)) /d
                 if self.position.y > object.position.y:
                     ay = ay * -1
-                az = quotient / pow((self.position.z - object.position.z),2)
+                az = a * abs((self.position.z - object.position.z)) / d
                 if self.position.z > object.position.z:
                     az = az * -1
 
@@ -85,5 +92,4 @@ def GenerateRandomObject(name) -> Object_with_mass:
     mass = random.rand(1) * mass_seed
     return Object_with_mass(name, tuple_3(position[0],position[1],position[2]), tuple_3(speed[0],speed[1],speed[2]), mass)
     #return Object_with_mass(name, tuple_3(position[0],position[1],position[2]), tuple_3(0,0,0), mass)
-
 
